e1e7f00ab086d0dd5f6901b6f603ceee04c6f2fd
Luke Drummond <lukedrummond@gmail.com>
2014-12-25 05:48:29 +0000

    fix #10

--- src/error.c
+++ src/error.c
@@ -8,41 +8,6 @@ static uint32_t debug_level = BMO_MESSAGE_INFO;
 static char msg[BMO_ERR_LEN] = {'\0'};
 static FILE * out_stream = NULL;
 
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <wincon.h>
-static int console_wasinit;
-static HANDLE console_handle;
-//The following should be CONSOLE_SCREEN_BUFFER_INFOEX so we can reset the background
-//easily, but the MinGW CRT doesn't have the function definition.
-static CONSOLE_SCREEN_BUFFER_INFO console_info; 
-
-static void color_setup(void)
-{
-	static int init;
-	if(init){
-		return;
-	}
-	console_wasinit = 1;
-	console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	GetConsoleScreenBufferInfo(console_handle, &console_info);
-}
-
-static const char *win_setcolor(WORD color)
-{
-	color_setup();
-	SetConsoleTextAttribute(console_handle, color);
-	return "";
-}
-
-static void win_resetcolor(void)
-{
-	SetConsoleTextAttribute(console_handle, console_info.wAttributes);
-}
-
-#endif
-
 void bmo_verbosity(uint32_t level)
 {
 	if (level > BMO_MESSAGE_DEBUG)
@@ -87,7 +52,6 @@ void _bmo_message(uint32_t level, const char * fn, const char * message, ...)
 		
 		vsnprintf(msg + len, (size_t)(BMO_ERR_LEN - 1 - len), message, argp);
 		fputs(msg, out_stream != NULL ? out_stream:stderr);
-		win_resetcolor();
 //		doprint = 0;
 	}
 	else if(level == BMO_MESSAGE_CRIT)
